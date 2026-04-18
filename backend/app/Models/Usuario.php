28a6b0217b4c780ef8bb7d893b0a003133eec79a
Alejand5672 <her241424@uvg.edu.gt>
2026-04-18 15:58:08 -0600

    Intento de implementación de backend falta revision

--- backend/app/Models/Usuario.php
+++ backend/app/Models/Usuario.php
@@ -2,9 +2,33 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
-class Usuario extends Model
+class Usuario extends Authenticatable
 {
-    //
+    use HasFactory, Notifiable, HasApiTokens;
+
+    protected $table = 'usuarios';
+
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'email_verified_at' => 'datetime',
+            'password' => 'hashed',
+        ];
+    }
 }
