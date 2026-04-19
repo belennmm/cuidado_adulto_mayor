0f5669742c89fbdf5aeb9fa219e50fa85e64ca6b
Chon211205 <lee241203@uvg.edu.gt>
2026-04-19 01:57:49 -0600

    rutas del api modificadas para que funcionen en servidor

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 Route::get('/ping', function () {
     return response()->json(['ok' => true]);
 });
+
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+
